16b06087aaffc547a502cb0bd153ae961a5c0a41
MN3TR <3397249463@qq.com>
2026-02-07 18:06:24 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,25 @@
-const CACHE_NAME = 'timer-v1';
-const assets = ['./', './index.html', './manifest.json'];
-
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+const CACHE_NAME = 'focus-v2';
+// 监听安装事件，缓存基础资源
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(['/', '/index.html']); 
+        })
+    );
 });
 
-self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+// 动态拦截：Stale-While-Revalidate 策略
+// 即使是 /src/ 下的文件也会被自动缓存
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then(cachedResponse => {
+            const fetchPromise = fetch(e.request).then(networkResponse => {
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(e.request, networkResponse.clone());
+                });
+                return networkResponse;
+            });
+            return cachedResponse || fetchPromise;
+        })
+    );
 });
---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // base: '/你的仓库名/', 
+  // 如果你的仓库名是 focus-timer，则写为 '/focus-timer/'
+  base: '/simper-clock/', 
+})
